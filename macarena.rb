e54c41a5500d4277291eda76e9dd92ad9fd0e9cd
nimetbetul <nbkaratas@students.chccs.k12.nc.us>
2018-07-23 20:21:18 +0000

    automatically backed up by learn

--- macarena.rb
+++ macarena.rb
@@ -25,4 +25,26 @@ puts "Step 6: Place your left hand on your right shoulder."
 puts "Step 7: Bring your right hand up and around to the back of your head."
 
 # Step 8: FIXME
-puts "Step 8: Bring your left hand up and around to the back your head."
+puts "Step 8: Bring your left hand up and around to the back yourdef Gangnam Style-Dance Moves-Lower Body
+puts "plant legs far apart, bend knees slightly and keep posture loose."
+puts "Left right foot."
+puts "Return right foot to the ground."
+puts "Finishing with a small skip-step backward."
+end
+
+def Gangnam Style- skip-step Left
+puts "Lower left foot to the ground."
+puts "Bounce left foot back up slightly, kicking it a few inches back."
+puts "left, Right, Left and Left."
+end
+
+def Gangnam Style- skip-step Right
+puts "Lower right foot to the ground."
+puts "Bounce right foot back up slightly, kicking it a few inches back."
+puts "Right, Left, Right and Right."
+end
+
+def Gangnam Style-Bob the Reins
+puts "Holding your arms out in front of you, straight and at chest level."
+puts "Cross your right wrist over you left and hold them together."
+puts "Lift your arms up and down in a loose bouncing movement, head."
